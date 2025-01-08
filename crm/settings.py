0107c33ef8b9eee8bbca22b06a941ad4925da5b2
Rizqulloh Rayhan Ferdiansyah <92198307+rizqullohrayhan@users.noreply.github.com>
2025-01-08 11:44:48 +0700

    change static file folder

--- crm/settings.py
+++ crm/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [BASE_DIR / 'assets']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
