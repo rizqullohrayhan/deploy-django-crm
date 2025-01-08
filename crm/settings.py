05d01e7a519fb0bfbc02e6c730d07bfa09a7bc71
Rizqulloh Rayhan Ferdiansyah <rizqullohrayhan@student.uns.ac.id>
2025-01-08 16:05:13 +0700

    change database to supabase

--- crm/settings.py
+++ crm/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_crm',
+        'USER': 'postgres',
+        'PASSWORD': 'eBLS.FjzkLX.Js4',
+        'HOST': 'db.ewsjnuzyvlvliwshpnwt.supabase.co',
+        'PORT': '5432',
     }
 }
 
